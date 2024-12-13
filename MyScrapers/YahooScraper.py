8c5463cc771ed9ed87410e3405df3d39876eb9c5
lie544 <145787178+rcbr230@users.noreply.github.com>
2024-12-13 01:29:32 -0500

    Changed to Yahoo Scraper
    Had to change to yahoo because amazon is to annoying to try and scrape...

--- MyScrapers/YahooScraper.py
+++ MyScrapers/YahooScraper.py
@@ -1,7 +1,7 @@
 """
 Author:  Reece Brogden
 Date:    December 12, 2024
-Purpose: Scrape HTML from amazon, and print out all the links and prices in console. As well as store 
+Purpose: Scrape HTML from Yahoo!Finance, and print out all the trending stocks in console. As well as store 
             results in a csv file. 
 """
 
@@ -12,12 +12,13 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 # urlInput = input() # take in a URL from the user
-urlInput = 'https://www.amazon.com/s?k=dave&i=stripbooks&crid=10HC1FJO3TGCV&sprefix=dave%2Cstripbooks%2C179&ref=nb_sb_noss_2'
+urlInput = 'https://finance.yahoo.com/markets/stocks/trending/'
 
 # simulate a user agent
 ua = UserAgent()
 randomUA = ua.random
 # NOTE: Amazon requires a User-Agent AND an Accept-Language!!
+#   I gave up on trying to do amazon...
 header = {
     'User-Agent' : str(randomUA),
     'Accept-Language' : 'en-US, en;q=0.5'
@@ -30,10 +31,29 @@ htmlContent = page.text
 
 # Use beautiful soup to easily find what I am searching for in the HTML
 soup = BeautifulSoup(page.content, 'html.parser')
-links = soup.find_all('span', class_='')
 
-for each in links:
-    print(each)
+# grab all company names that are on the stock page
+companies = soup.find_all('div', class_='tw-pl-4 yf-h8l7j7')
+# values order:
+#   price, change, change %, change AGAIN, change % AGAIN, Volume, Market Cap
+values = soup.find_all('fin-streamer')
+
+# format into a dict.
+results = {}
+comapnyIndex = 0
+for company in companies:
+    results[company.string] = {
+        'Price' : values[comapnyIndex].string,
+        'Change' : values[comapnyIndex+1].string,
+        'ChangePercent' : values[comapnyIndex+4].string,
+        'Volume' : values[comapnyIndex+5].string,
+        'Market Cap' : values[comapnyIndex+6].string
+    }
+    comapnyIndex += 1
+
+for i,j in results.items():
+    print(i)
+    print(j)
 
 # check for success, otherwise print error code
 if page.status_code == 200:
